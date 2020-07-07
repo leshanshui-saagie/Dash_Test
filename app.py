3b6b0c444bb0183274a431178914c975f3aa1a94
leshanshui-saagie <65134274+leshanshui-saagie@users.noreply.github.com>
2020-07-07 21:37:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 import os
 import dash
-
-from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
-
-
-import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import flask
