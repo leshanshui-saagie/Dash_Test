41bd3efc2b423bee15fb89797957566ede9db70a
leshanshui-saagie <65134274+leshanshui-saagie@users.noreply.github.com>
2020-07-19 22:30:32 +0200

    Update vis.py

--- vis.py
+++ vis.py
@@ -3,19 +3,18 @@ import dash_core_components
 import dash_html_components
 import numpy
 import plotly.graph_objs as go
-import flask
 
 
 t = 20
 x = numpy.linspace(0, 2 * numpy.pi, 100)
 y = 10 * 2 * numpy.cos(t)
 
-server = flask.Flask(__name__) # define flask app.server
-app = dash.Dash(__name__, server=server) # call flask server
+app = dash.Dash(__name__, requests_pathname_prefix="/heads/")
 
 app.scripts.config.serve_locally = True # Debug use
 app.css.config.serve_locally = True # Debug use
 app.config.suppress_callback_exceptions = True # Debug use
+app.routes_pathname_prefix = app.url_base_pathname (already had app.url_base_pathname = "/heads/") # Debug use
 
 app.layout = dash_html_components.Div(children=[
     dash_html_components.H1(children='Testme'),
