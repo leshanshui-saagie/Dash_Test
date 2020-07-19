7338db02216d1e6b03464d27a44facedc3812e5c
leshanshui-saagie <65134274+leshanshui-saagie@users.noreply.github.com>
2020-07-19 22:01:50 +0200

    Update vis.py

--- vis.py
+++ vis.py
@@ -2,15 +2,21 @@ import dash
 import dash_core_components
 import dash_html_components
 import numpy
+import plotly.graph_objs as go
+import flask
+
 
 t = 20
 x = numpy.linspace(0, 2 * numpy.pi, 100)
 y = 10 * 2 * numpy.cos(t)
 
-app = dash.Dash()
-server = app.server
+server = flask.Flask(__name__) # define flask app.server
+app = dash.Dash(__name__, server=server) # call flask server
+
 app.scripts.config.serve_locally = True # Debug use
 app.css.config.serve_locally = True # Debug use
+app.config.suppress_callback_exceptions = True # Debug use
+
 app.layout = dash_html_components.Div(children=[
     dash_html_components.H1(children='Testme'),
     dash_core_components.Graph(
@@ -20,5 +26,10 @@ app.layout = dash_html_components.Div(children=[
                 })
     ])
 
+
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8051)
+
+    
+
+
